a85eaaa9500d8d67c2ee76741d00db506b6b84e6
Joe86lam <92540646+Joe86lam@users.noreply.github.com>
2021-11-15 16:03:02 -0500

    button on android so far

--- AppIconChange/App.tsx
+++ AppIconChange/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import {
   Button,
+  NativeModules,
   SafeAreaView,
   ScrollView,
   StatusBar,
--- README.md
+++
@@ -1,2 +0,0 @@
-# RN-TS-AppIconChange
-Repo to practice some app icon changing in a blank app using the RN TS blank template
